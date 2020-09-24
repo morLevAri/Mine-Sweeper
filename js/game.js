d5f08a38cdcbc7afc270a1373d6bad3357cbaec9
morLevAri <71779002+morLevAri@users.noreply.github.com>
2020-09-24 20:35:42 +0300

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,8 +1,18 @@
 'use strict';
 console.log('Minesweeper');
 
-var mine = '💣';
+const MINE = '💣';
+const FLAG = '🚩';
+const LIFE = '💗';
 
+const HAPPY = '😃';
+const AFRAID = '😐';
+const LOSS = '😵';
+const WIN = '😎';
+
+var gLife = 4
+var gTimeInterval;
+var gTotalSeconds = -1;
 var gBoard;
 var gLevel = {
   SIZE: 4,
@@ -17,13 +27,21 @@ var gGame = {
 };
 
 function init() {
+  // if (!gGame.isOn) return
+
+  var elBtn = document.querySelector('.newGame');
+  elBtn.innerText = HAPPY;
   gBoard = buildBoard();
+  gGame.isOn = false;
+  var gLife = 4;
   renderBoard(gBoard);
+  clearInterval(gTimeInterval);
+  gTotalSeconds = -1;
+  setTime();
 }
 
 function buildBoard() {
   var board = [];
-  //  var currBoard = JSON.parse(JSON.stringify(gBoard));
   for (var i = 0; i < gLevel.SIZE; i++) {
     board[i] = [];
     for (var j = 0; j < gLevel.SIZE; j++) {
@@ -35,35 +53,38 @@ function buildBoard() {
       };
     }
   }
-
   console.table(board);
-  createBombs(board, gLevel.MINES);
   return board;
 }
 
-renderBoard(gBoard);
-
 function renderBoard(board) {
   console.table(board);
+
   var elBoard = document.querySelector('.board');
   var strHTML = '';
-  for (var i = 0; i < board.length; i++) {
+  for (var i = 0; i < gLevel.SIZE; i++) {
     strHTML += '<tr>\n';
-    for (var j = 0; j < board[0].length; j++) {
+    for (var j = 0; j < gLevel.SIZE; j++) {
       var currCell = board[i][j];
       var cellClass = getClassName({ i: i, j: j });
       strHTML += `\t<td class="`;
+
       if (!currCell.isShown) strHTML += 'hide';
-      strHTML += ` cell ${cellClass}" onclick="cellClicked(event, this,${i},${j})"  >\n`;
-      strHTML += '\t</td>\n';
+      if (currCell.isShown) strHTML += 'show';
+      var neighbors = setMinesNegsCount(i, j, board);
+      if (currCell.isMine && currCell.isShown) {
+        neighbors = MINE;
+        strHTML += ' bombShow';
+      }
+      strHTML += ` cell ${cellClass}" onclick="cellClicked(event, this,${i},${j})" 
+       oncontextmenu = "cellMarked(this,${i},${j})"> \n${neighbors} </td>`;
     }
-    strHTML += '</tr>\n';
+    strHTML += '\n</tr>\n';
     console.log(strHTML);
     elBoard.innerHTML = strHTML;
   }
   console.log(board);
 }
-//   board[2][1].gBoard.isShown = true;
 
 function setMinesNegsCount(cellI, cellJ, mat) {
   var neighborsSum = 0;
@@ -75,32 +96,42 @@ function setMinesNegsCount(cellI, cellJ, mat) {
       if (mat[i][j].isMine) neighborsSum++;
     }
   }
-  //   console.log(neighborsSum);
+  //console.log(neighborsSum);
   return neighborsSum;
 }
 
-function cellClicked(ev, theString, i, j) {
+function cellClicked(ev, str, i, j) {
+  if (!gGame.isOn) {
+    gTimeInterval = setInterval(setTime, 1000);
+    createBombs(gBoard, gLevel.MINES, i, j);
+    gGame.isOn = true;
+    renderBoard(gBoard);
+  }
+
   var currCell = gBoard[i][j];
   var neighbors = setMinesNegsCount(i, j, gBoard);
   console.log(neighbors);
-  theString.innerText = currCell.isMine ? mine : neighbors;
-  // currCell[i][j].gBoard.isShown = true;
 
-  setMinesNegsCount(i, j, board);
+  if (currCell.isMine) {
+    str.innerText = MINE;
+    currCell.isShown = true;    
+    renderBoard(gBoard); // לבדוק האם להציג את הפצצה מכאן>
+    GameOver();
+    
+  } else {
+    str.innerText = neighbors;
+    gBoard[i][j].isShown = true;
+    renderBoard(gBoard);
+    var elBtn = document.querySelector('.newGame');
+    elBtn.innerText = AFRAID;
+    setTimeout(function () {
+      elBtn.innerText = HAPPY;
+    }, 150);
+  }
+  expandShown(gBoard, currCell, i, j);
 }
 
-function cellMarked(elCell) {}
-
-function checkGameOver() {}
-
-function expandShown(board, elCell, i, j) {}
-
-function getClassName(location) {
-  var cellClass = 'cell-' + location.i + '-' + location.j;
-  return cellClass;
-}
-
-function createBombs(board, amount) {
+function createBombs(board, amount, cellI, cellJ) {
   var bomb = {
     minesAroundCount: 0,
     isShown: false,
@@ -110,7 +141,10 @@ function createBombs(board, amount) {
   for (let i = 0; i < amount; i++) {
     var num1 = getRandNum();
     var num2 = getRandNum();
-    if (!board[num1][num2].isMine) {
+    if (
+      !board[num1][num2].isMine &&
+      board[num1][num2] !== board[cellI][cellJ]
+    ) {
       board[num1][num2] = bomb;
     } else {
       i--;
@@ -122,4 +156,61 @@ function changeLevel(size, mines) {
   gLevel.SIZE = size;
   gLevel.MINES = mines;
   init();
+}
+
+function GameOver() {
+  
+  gLife--;
+  var elLives = document.querySelector('.lives');
+  elLives.innerText = '';
+  for (var i = 0; i < gLife; i++) {
+  elLives.innerText += '💗 ' ;
+  }
+  
+  var elBtn = document.querySelector('.newGame');
+  elBtn.innerText = LOSS;
+
+  if (gLife === 0){
+  //חשיפת המוקשים
+  for (var i = 0; i < gLevel.SIZE; i++) {
+    for (var j = 0; j < gLevel.SIZE; j++) {
+      if (gBoard[i][j].isMine) gBoard[i][j].isShown = true;
+    }
+  }
+  alert('you lost')
+  } 
+}
+
+
+
+
+
+
+
+function cellMarked(str, i, j) {
+  // disableMenu();
+  if (!gGame.isOn) {
+    gTimeInterval = setInterval(setTime, 1000);
+    gGame.isOn = true;
+    renderBoard(gBoard);
+  }
+  var currCell = gBoard[i][j];
+  str.innerText = FLAG;
+
+
+
+}
+
+function expandShown(board, elCell, cellI, cellJ) {
+  var neighbors = setMinesNegsCount(cellI, cellJ, board);
+  if (neighbors === 0) {
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+      if (i < 0 || i >= board.length) continue;
+      for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+        if (j < 0 || j >= board[i].length) continue;
+        if (i === cellI && j === cellJ) continue;
+        board[i][j].isShown = true;
+      }
+    }
+  }
 }
--- js/util.js
+++ js/util.js
@@ -4,3 +4,38 @@ function getRandNum() {
   var rand = Math.floor(Math.random() * gLevel.SIZE);
   return rand;
 }
+
+
+
+function getClassName(location) {
+  var cellClass = 'cell-' + location.i + '-' + location.j;
+  return cellClass;
+}
+
+
+
+function setTime() {
+  var minutesLabel = document.getElementById('minutes');
+  var secondsLabel = document.getElementById('seconds');
+  gTotalSeconds++;
+  secondsLabel.innerHTML = pad(gTotalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(gTotalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + '';
+  if (valString.length < 2) {
+    return '0' + valString;
+  } else {
+    return valString;
+  }
+}
+
+
+
+
+// function disableMenu() {
+//   document.addEventListener("contextmenu", function(a){
+//       a.preventDefault();
+//   })
+// }
